3b8e7146c0cbadea5454ed074dec4562d834e159
Tung Nguyen <tongueroo@gmail.com>
2019-06-02 23:53:20 +0000

    init commit

---
+++ lib/cfn/status/version.rb
@@ -0,0 +1,5 @@
+module Cfn
+  class Status
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/cfn/status_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cfn::Status do
+  it "has a version number" do
+    expect(Cfn::Status::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
